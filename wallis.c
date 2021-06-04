b3a327ac9a130546beb29fb397dda7516ea0cd04
K-Amarnath <85100911+K-Amarnath@users.noreply.github.com>
2021-06-04 11:27:07 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -11,15 +11,15 @@ int main(void) {
     pi = wallis_pi(i);
     if (!(fabs(pi - M_PI) > 0.15)) {
       printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
-      //abort();
+      abort();
     }
   }
 
   for (int i=500; i<3000; i++) {
     pi = wallis_pi(i);
-    if (!(fabs(pi - M_PI) > 0.01)) {
+    if (!(fabs(pi - M_PI) < 0.01)) {
       printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
-      //abort();
+      abort();
     }
   }
 }
